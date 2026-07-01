c38a84051ff843be0ea2c3fd4cbc9344c8536270
danieldidas103-hub <danieldidas103@gmail.com>
2026-07-01 17:20:57 +0300

    Create bmi_calculator.py
    First version of BMI calculator with if/elif/else logic

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -1,1 +1,19 @@
+#python body mass index BMI calculator
 
+weight=float(input("enter you are weight in kg: "))
+
+height=float(input("enter you are height in meters: "))
+
+BMI=weight / (height**2)
+
+if BMI <=18.5:
+	print("the patient is underweight")
+
+elif BMI<=25:
+	print("the patient is normal heathly ")
+
+elif BMI<=30:
+	print("the patient is overweight")
+	
+else:
+	print("the patient is obese")
