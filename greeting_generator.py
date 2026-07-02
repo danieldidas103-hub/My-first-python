de464437839960119e177b310feb2aa3a5a1c019
danieldidas103-hub <danieldidas103@gmail.com>
2026-07-02 15:03:31 +0300

    Python if / elif / else practice project
    # Tanzania Greeting Generator 🇹🇿
    
    A beginner Python project built using `if/elif/else`, `user input`, and `strings`.
    
    ## About Me
    I am Daniel, an Aspiring Computer Science Student from Tanzania. 
    My goal: Study Computer Science at UDSM, then Data Science via Erasmus.
    
    ## What This Project Does
    Asks the user for a time of day: `morning`, `afternoon`, or `night`.
    Then prints a Swahili greeting + welcome message.
    Also handles invalid input.
    
    ## Skills Used
    - Python Basics: variables, input(), typecasting
    - Control Flow: if / elif / else statements 
    - Strings: f-strings and escape character `\n`
    
    ## How to Run
    1. Clone this repo
    2. Run: `python greeting_generator.py`
    3. Type: morning, afternoon, or night
    
    ## Next Goal
    Project : UDSM GPA Calculator - Coming Soon
    
    ---
    **Building in public. 1 project at a time.**

---
+++ greeting_generator.py
@@ -0,0 +1,16 @@
+#TANZANIA GREETING GENERATOR
+#Author: Daniel - asipiring computer science student, Tanzania
+#Goal UDSM computer science ~~ erasmus data science
+time=input("what time is now (morning, afternoon,  night) : ")
+
+if time== "morning":
+	print("habari ya asubuhi, Daniel! \n welcome to data science ")
+	
+elif time== "afternoon":
+	print("habari ya mchana, Daniel! \n welcome to data science")
+	
+elif time== "night":
+	print("habari ya usiku, Daniel! \n welcome to data science ")
+	
+else:
+	print("time entered is invalid")
